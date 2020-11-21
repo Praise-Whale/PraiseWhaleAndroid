11b28a6d2d5658c6cfb26f0b2827a8cab0af2eea
annayoung <dpswpfskdud@naver.com>
2020-11-22 08:14:00 +0900

    +level

--- app/src/main/java/com/example/praisewhale/CollectionImpl.kt
+++ app/src/main/java/com/example/praisewhale/CollectionImpl.kt
@@ -1,5 +1,6 @@
 package com.example.praisewhale
 
+import com.example.praisewhale.api.RequestInterface
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
--- app/src/main/java/com/example/praisewhale/api/RequestInterface.kt
+++ app/src/main/java/com/example/praisewhale/api/RequestInterface.kt
@@ -1,10 +1,10 @@
-package com.example.praisewhale
+package com.example.praisewhale.api
 
+import com.example.praisewhale.RequestCollectionData
+import com.example.praisewhale.ResponseCollectionData
+import com.example.praisewhale.ResponseUserData
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.Headers
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface RequestInterface {
     //최근 칭찬 유저 조회
@@ -27,4 +27,11 @@ interface RequestInterface {
     fun postUsers(
         @Body body : RequestCollectionData
     )  : Call<ResponseCollectionData>
+
+    // 레벨 조회
+    @Headers("Content-Type:application/json")
+    @GET("level/praise/{userIdx}")
+    fun getuserIdx(
+        @Path("userIdx") userIdx : Int
+    )  : Call<ResponseUserData>
 }
