6cc94518306552b139f768b3cfc02aef6ef157e7
yxnsx <cw2cu96@gmail.com>
2020-11-22 00:54:18 +0900

    Add: 메인 탭 UI 구현 완료
    * 상세 내용
    - 메인 탭 UI 구현
    - positive 버튼 클릭시 다이얼로그 출력
    - negative 버튼 클릭시 다이얼로그 출력

--- app/src/main/java/com/example/praisewhale/fragment/MainFragment.kt
+++ app/src/main/java/com/example/praisewhale/fragment/MainFragment.kt
@@ -1,21 +1,89 @@
 package com.example.praisewhale.fragment
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
+import android.widget.TextView
 import com.example.praisewhale.R
+import kotlinx.android.synthetic.main.dialog_positive.*
+import kotlinx.android.synthetic.main.fragment_main.*
 
 
 class MainFragment : Fragment() {
+
+    private lateinit var alertDialog: AlertDialog
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    // ui 작업 수행
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        textView_mainPraiseMission.text = "칭찬 내용 / 칭찬 내용 / 칭찬 내용 / 칭찬 내용 / 칭찬 내용 / "
+        Log.d("TAG", "onViewCreated: ${textView_mainPraiseMission.text}")
+        button_mainPositive.setOnClickListener(fragmentOnClickListener)
+        button_mainNegative.setOnClickListener(fragmentOnClickListener)
+    }
+
+    private val fragmentOnClickListener = View.OnClickListener {
+        when (it.id) {
+            R.id.button_mainPositive -> {
+                showPositiveDialog()
+            }
+            R.id.button_mainNegative -> {
+                showNegativeDialog()
+                Log.d("TAG", "buttonNegative: clicked")
+            }
+        }
+    }
+
+    private fun showPositiveDialog() {
+        val alarDialogBuilder = AlertDialog.Builder(context)
+            .setView(layoutInflater.inflate(R.layout.dialog_positive, null))
+            .setPositiveButton("확인", dialogClickListener)
+            .setNegativeButton("취소", dialogClickListener)
+
+        alertDialog = alarDialogBuilder.create()
+        alertDialog.show()
+        alertDialog.radioGroup_dialogRecentPraiseTo.setOnCheckedChangeListener(radioGroupListener)
+
+    }
+
+    private fun showNegativeDialog() {
+        val alarDialogBuilder = AlertDialog.Builder(context)
+            .setView(layoutInflater.inflate(R.layout.dialog_negative, null))
+            .setPositiveButton("확인", dialogClickListener)
+
+        alertDialog = alarDialogBuilder.create()
+        alertDialog.show()
+    }
+
+    private val dialogClickListener = DialogInterface.OnClickListener { dialog, which ->
+        when (which) {
+            DialogInterface.BUTTON_POSITIVE -> {
+                alertDialog.editText_dialogPraiseTo?.setText("")
+                alertDialog.radioGroup_dialogRecentPraiseTo?.clearCheck()
+            }
+            DialogInterface.BUTTON_NEGATIVE -> {
+
+            }
+        }
+    }
+
+    private val radioGroupListener = RadioGroup.OnCheckedChangeListener { group, checkedId ->
+//        checkCategoryColorValidation(group)
+//        alartDialog.getButton(DialogInterface.BUTTON_POSITIVE).isEnabled = checkDialogValidation()
     }
 }
