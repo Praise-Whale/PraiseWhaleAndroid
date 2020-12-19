f2ac8c91b27f2591f3e9b7c79c96df5cd4160f25
annayoung <dpswpfskdud@naver.com>
2020-12-20 00:18:11 +0900

    setting +progressbar

---
+++ app/src/main/java/com/example/praisewhale/ResponselevelData.kt
@@ -0,0 +1,16 @@
+package com.example.praisewhale
+
+data class ResponselevelData (
+    val status : Int,
+    val message : String,
+    val data : Data
+){
+    data class Data(
+        val nickName : String,
+        val whaleName : String,
+        val userLevel : Int,
+        val praiseCount : Int,
+        val levelUpNeedCount : Int
+    )
+
+}
