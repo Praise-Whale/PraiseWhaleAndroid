a82e3ab3f394f56ec4cc5bdf8409a8e628198f65
ny2060 <dpswpfskdud@naver.com>
2021-02-20 16:06:43 +0900

    [feat]:custom_toast

---
+++ app/src/main/java/com/example/praisewhale/InfoActivity.kt
@@ -0,0 +1,14 @@
+package com.example.praisewhale
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_developer.*
+
+class InfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/praisewhale/InfoUserActivity.kt
@@ -0,0 +1,13 @@
+package com.example.praisewhale
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class InfoUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_user)
+
+
+    }
+}
