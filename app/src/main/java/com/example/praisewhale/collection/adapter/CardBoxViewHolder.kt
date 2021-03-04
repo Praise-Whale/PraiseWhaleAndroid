9eb1a6fb467a82b6a06e581d047cf5cc8b2ea74e
yunso <cw2cu96@gmail.com>
2021-03-05 00:36:14 +0900

    Fix: CardFragment.kt 파일의 리사이클러뷰에 설정한 첫번째 아이템 마진값 삭제

--- app/src/main/java/com/example/praisewhale/collection/adapter/CardBoxViewHolder.kt
+++ app/src/main/java/com/example/praisewhale/collection/adapter/CardBoxViewHolder.kt
@@ -1,9 +1,7 @@
 package com.example.praisewhale.collection.adapter
 
-import android.content.res.Resources
 import android.view.View
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.praisewhale.R
 import com.example.praisewhale.collection.data.CollectionPraise
 
@@ -12,8 +10,7 @@ class CardBoxViewHolder (itemView : View) : androidx.recyclerview.widget.Recycle
     private val createdAt : TextView = itemView.findViewById(R.id.tv_card_date)
     private val todayPraise : TextView = itemView.findViewById(R.id.tv_card_praise)
 
-    fun onBind(position: Int, data: CollectionPraise){
-        setFirstItemMargin(position)
+    fun onBind(data: CollectionPraise){
         praisedName.text = ellipsis(data.praisedName, 5, "...")
         createdAt.text = data.created_at.substring(5,7).toInt().toString()+"월 "+data.created_at.substring(8,10).toInt().toString()+"일"
         todayPraise.text = data.today_praise
@@ -26,13 +23,5 @@ class CardBoxViewHolder (itemView : View) : androidx.recyclerview.widget.Recycle
         if (length <= maxLength) { return str }
 
         return str.substring(0, maxLength) + ellipsis
-    }
-
-    private fun setFirstItemMargin(position: Int) {
-        if (position == 0) {
-            val layoutParams = ConstraintLayout.LayoutParams(0, 0)
-            layoutParams.setMargins((32 * Resources.getSystem().displayMetrics.density).toInt(), 0, 0, 0)
-            itemView.rootView.layoutParams = layoutParams
-        }
     }
 }
