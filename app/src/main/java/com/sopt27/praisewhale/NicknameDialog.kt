54ab102e6f099df8d51b1fbc15e74a9869b55784
안나영 <dpswpfskdud@naver.com>
2021-03-30 17:49:25 +0900

    praiselevelfragment 리팩토링

--- app/src/main/java/com/sopt27/praisewhale/NicknameDialog.kt
+++ app/src/main/java/com/sopt27/praisewhale/NicknameDialog.kt
@@ -22,16 +22,6 @@ class NicknameDialog (context : Context) {
         dlg.setContentView(R.layout.namechange)     //다이얼로그에 사용할 xml 파일을 불러옴
         dlg.setCancelable(false)    //다이얼로그의 바깥 화면을 눌렀을 때 다이얼로그가 닫히지 않도록 함
 
-        /*lblDesc = dlg.findViewById(R.id.content)
-
-        btnOK = dlg.findViewById(R.id.change_btn)
-        btnOK.setOnClickListener {
-
-            listener.onOKClicked("확인을 눌렀습니다")
-            dlg.dismiss()
-        }
-
-*/
 
         dlg.show()
     }
